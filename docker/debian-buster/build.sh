3db48785136ec3e9badff8877a872312a782672e
segfault <segfault@riseup.net>
2021-02-17 23:43:33 +0100

    Create Docker images for use in GitLab CI jobs
    ... which have the packages required by (most of) the CI jobs already
    installed.

---
+++ docker/debian-buster/build.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -euo pipefail
+set -x
+
+print_usage() {
+  cat << EOF
+usage: $0 [OPTIONS]
+
+Build the docker image.
+
+Options:
+--push
+    Push the image after building it.
+-h, --help
+    Print usage message.
+EOF
+}
+
+# Parse arguments
+while [ "$#" -gt 0 ]; do
+case "$1" in
+    --push)
+    PUSH=1
+    shift
+    ;;
+    -h|--help)
+    print_usage
+    exit 0
+    ;;
+    *) # unknown option
+    echo "unknown option: $1"
+    print_usage
+    exit 1
+    ;;
+esac
+done
+
+TAG="${TAG:-"tailslive/debian-buster:v1"}"
+
+# Build the image
+DOCKER_BUILDKIT=1 docker build \
+ --cache-from "${TAG}" \
+ --build-arg BUILDKIT_INLINE_CACHE=1 \
+ --tag "${TAG}" \
+ .
+
+# Push the image (if requested)
+if [ -n "${PUSH:-}" ]; then
+  docker push "${TAG}"
+fi
