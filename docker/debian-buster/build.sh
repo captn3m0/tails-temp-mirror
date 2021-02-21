4e51ef9eb8ee7c07d427846229fc28b367704835
segfault <segfault@riseup.net>
2021-02-21 19:24:22 +0100

    WIP: Introduce a LATEST file
    I'm now unsure if this is a good way to go

--- docker/debian-buster/build.sh
+++ docker/debian-buster/build.sh
@@ -36,16 +36,29 @@ case "$1" in
 esac
 done
 
-TAG="${TAG:-"tailslive/debian-buster:v1"}"
+# Import BASE_IMAGE and BASE_IMAGE_DIGEST
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+source "${SCRIPT_DIR}/LATEST"
+
+# Pull the latest version of the base image
+docker pull "${BASE_IMAGE}"
+
+# 
+
+TAG="${TAG:-"tailslive/debian-buster:${BASE_IMAGE_DIGEST:0:8}"}"
 
 # Build the image
 DOCKER_BUILDKIT=1 docker build \
  --cache-from "${TAG}" \
  --build-arg BUILDKIT_INLINE_CACHE=1 \
+ --build-arg BASE_IMAGE_DIGEST="${BASE_IMAGE_DIGEST}" \
  --tag "${TAG}" \
  .
 
 # Push the image (if requested)
 if [ -n "${PUSH:-}" ]; then
   docker push "${TAG}"
+
+  # Update the BASE_IMAGE_DIGEST in the LATEST file
+
 fi
